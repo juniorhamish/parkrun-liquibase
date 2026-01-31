12ece5e058e22e8ffa28fb3a5c6d9ed667518cef
David Johnston <davidajohnston2000@gmail.com>
2026-01-31 23:08:16 +0000

    Add initial Liquibase setup with schema creation and seed data

---
+++ migrations/changelog-table.sql
@@ -0,0 +1,21 @@
+--liquibase formatted sql
+
+--changeset David Johnston:1
+--comment: Add runners table
+CREATE TABLE runners
+(
+    id   text PRIMARY KEY,
+    name text
+);
+--rollback DROP TABLE runners;
+
+--changeset David Johnston:2
+--comment: Add last scrape metadata table
+CREATE TABLE last_scrape_metadata
+(
+    id                    UUID PRIMARY KEY     DEFAULT gen_random_uuid(),
+    last_scrape_time      TIMESTAMPTZ NOT NULL,
+    new_parkrunners_count INTEGER     NOT NULL DEFAULT 0,
+    success               BOOLEAN     NOT NULL DEFAULT TRUE
+);
+--rollback DROP TABLE last_scrape_metadata;
